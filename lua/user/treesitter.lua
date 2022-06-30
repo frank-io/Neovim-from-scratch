4648befe3adaf734cd5a4c03574b89c48d4ee203
Frank Wang <frank.wang@n3hub.com>
2022-07-01 11:13:48 +1200

    fix ensure install

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,21 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-local configs = require("nvim-treesitter.configs")
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
 configs.setup {
-  ensure_installed = "maintained",
-  sync_install = false, 
+  ensure_installed = "all",
+  sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
@@ -24,4 +13,12 @@ configs.setup {
 
   },
   indent = { enable = true, disable = { "yaml" } },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  } 
 }
